1b2b30ef9b9c82ad84954befbf663289468c157e
MercyNJ <mercinjuguna@gmail.com>
2023-03-02 03:53:34 -0800

    fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints sum of even fibonacci numbers
+ * less than 4,000,000.
+ *
+ * Return: Nothing
+ */
+
+int main(void)
+
+{
+	int i = 0;
+	long j = 1;
+	long k = 2;
+	long sum = k;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
+		j = k - j;
+		++i;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
