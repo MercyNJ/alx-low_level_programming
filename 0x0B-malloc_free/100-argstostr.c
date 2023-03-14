3165c1f1aa51b99c0d40dac32ef5863773d60a4f
MercyNJ <mercinjuguna@gmail.com>
2023-03-14 06:48:51 -0700

    concatenating arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - Concatenates arguments
+ * @ac: Grid width
+ * @av: Grid height
+ *
+ * Return: Concatenated arguments per line
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int count = 0;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (a < ac)
+	{
+		b = 0;
+		while (av[a][b] != '\0')
+		{
+			count++;
+			b++;
+		}
+		a++;
+	}
+	count = count + ac + 1;
+	str = malloc(sizeof(char) * count);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			str[c] = av[a][b];
+			c++;
+		}
+		str[c] = '\n';
+		c++;
+	}
+	return (str);
+}
