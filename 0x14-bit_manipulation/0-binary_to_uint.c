e704ce3c288e00cba321f27ee51584d8e74c9968
MercyNJ <mercinjuguna@gmail.com>
2023-04-06 04:45:58 -0700

    Correction of function converting binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,22 +11,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
+	unsigned int res, pow;
+	int length;
 
 	if (b == NULL)
-	{
 		return (0);
+
+	for (length = 0; b[length]; length++)
+	{
+		if (b[length] != '0' && b[length] != '1')
+			return (0);
 	}
 
-	while (*b != '\0')
+	for (pow = 1, res = 0, length--; length >= 0; length--, pow *= 2)
 	{
-		if (*b != '0' && *b != '1')
-		{
-			return (0);
-		}
-		res <<= 1;
-		res += (*b - '0');
-		b++;
+		if (b[length] == '1')
+			res += pow;
 	}
 	return (res);
 }
