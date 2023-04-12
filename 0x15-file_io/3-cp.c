d9c2b7e7c7130eae2161e109bc3d328bc7bdb247
MercyNJ <mercinjuguna@gmail.com>
2023-04-12 02:11:42 -0700

    Correction of a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,22 +3,22 @@
 
 /**
  * error_check - Checks if a file can be opened.
- * @srcFile: The source file.
- * @destFile: The destination file.
+ * @file_from: The source file.
+ * @file_to: The destination file.
  *
  * @argv: Arguments vector.
  *
  * Return: Nothing.
  */
 
-void error_check(int srcFile, int destFile, char *argv[])
+void error_check(int file_from, int file_to, char *argv[])
 {
-	if (srcFile == -1)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (destFile == -1)
+	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
@@ -35,42 +35,42 @@ void error_check(int srcFile, int destFile, char *argv[])
 
 int main(int argc, char *argv[])
 {
-	int srcFile, destFile, close_result;
+	int file_from, file_to, close_result;
 	ssize_t read_count, write_count;
 	char buff[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp srcFile destFile");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	srcFile = open(argv[1], O_RDONLY);
-	destFile = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_check(srcFile, destFile, argv);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_check(file_from, file_to, argv);
 
 	read_count = 1024;
 	while (read_count == 1024)
 	{
-		read_count = read(srcFile, buff, 1024);
+		read_count = read(file_from, buff, 1024);
 		if (read_count == -1)
 			error_check(-1, 0, argv);
-		write_count = write(destFile, buff, read_count);
+		write_count = write(file_to, buff, read_count);
 		if (write_count == -1)
 			error_check(0, -1, argv);
 	}
 
-	close_result = close(srcFile);
+	close_result = close(file_from);
 	if (close_result == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", srcFile);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	close_result = close(destFile);
+	close_result = close(file_to);
 	if (close_result == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", destFile);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
 	return (0);
